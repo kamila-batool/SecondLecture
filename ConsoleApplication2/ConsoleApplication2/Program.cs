95f1dd4aeb430afbbb5d44a65444004e4f360609
asp <asp@Aptechnk.edu>
2019-09-21 10:38:38 +0500

    first project

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -11,6 +11,8 @@ namespace ConsoleApplication2
         static void Main(string[] args)
         {
             Console.WriteLine("Our First Project");
+            Console.WriteLine("updated project");
+
         }
     }
 }
